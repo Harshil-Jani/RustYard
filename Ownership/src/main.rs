2fa1e8e7bc2fd9a5b5322fb73ed36e09371cc4f0
Harshil Jani <harshiljani2002@gmail.com>
2022-02-03 22:17:00 +0530

    Reference, Slices Added

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -74,6 +74,69 @@ fn main(){
         // println!("{}",s2) --> Won't Work as Ownership of S2 is taken.
         println!("{}",s3);
     }
+
+   // -------------------------------------References and Borrowing-----------------------------------------------
+    
+   // This is definately not the way I want things to work. This is cumbersome. So References and Alias will help me get over.
+    {
+        let s1 = String::from("Phenomenality");
+        let (s2,len) = calculate_length(s1);  
+        println!("The length of {} is {}",s2,len);
+    }
+    // Optimized Method of Above Implementation
+    {
+        let s1 = String::from("Incarnation");
+        let length = calculate_length_again(&s1);
+        println!("The length of {} is {}",s1,length);
+    }
+
+    {
+        let mut s1 = String::from("Linux");
+        change(&mut s1);
+        println!("{}",s1);
+    }
+    {
+        let mut s = String::from("Open Source");
+        let r1 = &mut s;
+        let r2 = &mut s;
+       // println!("{},{}",r1,r2); This fails because a same mutable variable cannot be borrowed by more than one values.
+    }
+    // To fix the above problem change the mutable variable to immutable so you can borrow it more than once.
+    {
+        let mut s = String::from("Open Source");
+        let r1 = &s; // The immutable reference of S, taken by r1 starts here.
+        let r2 = &s; // The immutable reference of S, taken by r2 starts here.
+        println!("{},{}",r1,r2); // The immutable reference of S, taken by r1 and r2 ends here.
+        let r3 = &mut s; // As the immutable reference was ended in above line, You can start mutable reference.
+        println!("{}",r3);// It was not possible to execute it in between when it was not ended.
+    }
+     /*
+    ------------References Rules in Rust----------
+    1. At any given time you can have either one mutable reference or any number of immutable reference in a particular scope.
+    2. References must be valid. (refer dangle function below)
+    */
+    {
+        //let reference_to_nothing = dangle();
+        //println!("{}",reference_to_nothing);
+    }
+
+    /*
+    -------------------------------------------Slicing------------------------------
+    Slices let you reference a contiguous sequence of elements in a collection
+    rather than the whole collection. A slice is a kind of reference, so it does not have ownership.
+    */
+    {
+        let s1 = String::from("Harshil Jani");
+        let word = first_word_index(&s1);
+        let name = &s1[..word];
+        let surname = &s1[word+1..];
+        println!("{} {}",name,surname);
+    }
+    {
+        let s1 = String::from("Harshil Jani");
+        let first_name = first_word(&s1);
+        println!("{}",first_name);
+    }
 }
 
 fn takes_ownerShip(s: String){
@@ -93,3 +156,51 @@ fn takes_and_gives_back(s: String) -> String{
     let something = s;
     something
 }
+
+fn calculate_length(s: String) -> (String, usize){
+    let length = s.len();
+    (s,length)
+}
+
+fn calculate_length_again(s: &String) -> usize{
+    let length = s.len();
+    length
+}
+
+fn change(s: &mut String){
+    s.push_str(" Foundation");
+}
+
+/*
+fn dangle() -> &String{
+    let s = String::from("Harshil"); // The reference to S gets dropped here itself as the scope ends.
+    &s // Cannot return anything that is memory waste.
+}
+*/
+
+fn first_word_index(s: &String) -> usize{
+    let bytes = s.as_bytes();
+    for (index,&items) in bytes.iter().enumerate(){
+        if items== b' '{
+            return index;
+        }
+    }
+    s.len()
+}
+
+fn first_word(s: &String) -> &str{
+    let bytes = s.as_bytes();
+    for (index,&items) in bytes.iter().enumerate(){
+        if items == b' '{
+            return &s[0..index];
+        }
+    }
+    s
+}
+
+/*
+The concepts of ownership, borrowing, and slices ensure memory safety in Rust programs at compile time.
+The Rust language gives you control over your memory usage in the same way as other systems programming 
+languages, but having the owner of data automatically clean up that data when the owner goes out of scope 
+means you don’t have to write and debug extra code to get this control.
+*/
